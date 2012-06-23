74d7d3a4300e2e08ecc8027a02941bd303960257
James Dunwoody <jamesdunwoody@gmail.com>
2012-06-23 17:22:39 +1000

    Initial Commit

---
+++ SampleMultiview/SampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SampleAppDelegate.h
+//  SampleMultiview
+//
+//  Created by James on 23/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
